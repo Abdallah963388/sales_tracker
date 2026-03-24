db42e820b3e33b4e5f37e06d873373ca1f82836b
Abdullah <a.gamal@sitksa-eg.com>
2026-03-24 12:45:06 +0200

    feat: update app name and version, add new dependencies, and register plugins
    - Changed app name from "Sales Tracker" to "SIT Sales Tracker" in Info.plist.
    - Updated version number in pubspec.yaml from 1.0.0+1 to 1.0.0+4.
    - Added dependencies: package_info_plus, flutter_animate, file_picker, hive, hive_flutter, permission_handler, path_provider, open_filex, and device_info_plus.
    - Registered new plugins in macOS and Windows generated files for permission_handler.
    - Updated asset path for app icon in icons_launcher configuration.
    - Commented out unused Hive initialization code in main.dart.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,10 +1,27 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
+import java.util.Properties
+import java.io.FileInputStream
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
 
+if (localPropertiesFile.exists()) {
+    localProperties.load(FileInputStream(localPropertiesFile)) 
+}
+
+val flutterVersionCode = localProperties.getProperty("flutter.versionCode")?.toIntOrNull() ?: 1
+val flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "1.0"
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    FileInputStream(keystorePropertiesFile).use {
+        keystoreProperties.load(it)
+    }
+}
 android {
     namespace = "com.sit.sales_tracker"
     compileSdk = flutter.compileSdkVersion
@@ -20,21 +37,27 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.sit.sales_tracker"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it.toString()) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            isShrinkResources = true
         }
     }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import '/../core/init/initializer.dart';
 import '/../features/my_app/my_app.dart';
 
 void main() async {
+  // await Hive.initFlutter();
+  // Hive.registerAdapter(ClientModelAdapter());
+  // await Hive.openBox<ClientModel>('clientsBox');
   await initializeApp();
   runApp(const MyApp());
 }
